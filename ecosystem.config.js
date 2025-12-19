8250b6af82dbd7bf27c51c0b0349e341c005fc41
Dushime20 <fabdushime20@gmail.com>
2025-12-19 15:09:13 +0200

    pm2 solve issue

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,16 +2,19 @@ module.exports = {
   apps: [{
     name: 'urutibiz-backend',
     script: 'dist/server.js',
-    cwd: '/root/urutibz',
-    node_args: '-r tsconfig-paths/register',
+    cwd: '/root/urutibz/urutibiz-backend',
     instances: 1,
+    exec_mode: 'fork',
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'production',
       PORT: 5000
-    }
+    },
+    error_file: '/root/.pm2/logs/urutibiz-backend-error.log',
+    out_file: '/root/.pm2/logs/urutibiz-backend-out.log',
+    log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+    merge_logs: true
   }]
 };
-
